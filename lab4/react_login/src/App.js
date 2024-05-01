d417ac8c3628f1577a743360d75d5c99490c80d2
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2024-05-01 16:49:03 +0800

    Add files via upload

---
+++ lab4/react_login/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+import './App.css';
+import CGU_login from './cgu_login';
+
+function App() {
+  return (
+    <div className="App">
+      {CGU_login()}
+    </div>
+  );
+}
+
+export default App;
