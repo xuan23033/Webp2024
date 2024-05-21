41e03e2c20031383f57df8e6a87e547d46ba241b
xuan23033 <113535717+xuan23033@users.noreply.github.com>
2024-05-22 00:31:31 +0800

    Add files via upload

---
+++ HW4/react_MyDatepickers/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import MyDatePickers from './mydatepicker.js';
+function App() {
+  return (
+    <div className="App">
+      <MyDatePickers/>
+    </div>
+  );
+}
+
+export default App;
